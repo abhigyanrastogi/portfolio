8427cd4aba744b8490922120959270eec6f61a8e
Abhigyan Rastogi <abhigyanrastogi0@gmail.com>
2026-05-12 12:34:41 +0530

    button based loading

--- src/components/ShowCaseProjectCarousel.tsx
+++ src/components/ShowCaseProjectCarousel.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { PingPongProject } from "./projects/PingPongProject"
 
 export const ShowCaseProjectCarousel = () => {
-
+    const [showPingPong, setShowPingPong] = useState(false);
+    const handleShowPingPong = () => {
+        setShowPingPong(true);
+    }
   return (
     <>
     <div>ShowCaseProjectCarousel</div>
-    <PingPongProject />
+    <button onClick={handleShowPingPong}>Show Ping Pong Project</button>
+    {showPingPong && <PingPongProject />}
     </>
     
   )
